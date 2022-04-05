a749f883f664d310fd14c8b4bb5dc87a1378462f
Sergan2B <IASer01gan2B>
2022-04-05 18:32:08 +0600

    Update: add TabLayout.tab extension

--- app/src/main/java/kg/geektech/kotlinapplicationhomework4/MainActivity.kt
+++ app/src/main/java/kg/geektech/kotlinapplicationhomework4/MainActivity.kt
@@ -1,12 +1,13 @@
 package kg.geektech.kotlinapplicationhomework4
 
 import android.view.LayoutInflater
-import androidx.core.content.ContextCompat
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kg.geektech.kotlinapplicationhomework4.adapter.PagerAdapter
 import kg.geektech.kotlinapplicationhomework4.base.BaseActivity
 import kg.geektech.kotlinapplicationhomework4.databinding.ActivityMainBinding
+import kg.geektech.kotlinapplicationhomework4.extentions.setIconAlpha
+import kg.geektech.kotlinapplicationhomework4.extentions.setIconColor
 
 /*
 - Добавить ViewPager2 из 3 фрагментов в Главном Активити +++++
@@ -34,27 +35,27 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             when (position) {
                 0 -> {
                     tab.setIcon(R.drawable.ic_baseline_confirmation_number_24)
-                    tab.icon?.setTint(ContextCompat.getColor(this, R.color.teal_200))
+                    setIconColor(tab, R.color.purple_500)
                 }
                 1 -> {
                     tab.setIcon(R.drawable.ic_baseline_countertops_24)
-                    tab.icon?.setTint(ContextCompat.getColor(this, R.color.purple_500))
-                    tab.icon?.alpha = 70
+                    setIconColor(tab, R.color.teal_200)
+                    setIconAlpha(tab, 70)
                 }
                 else -> {
                     tab.setIcon(R.drawable.ic_baseline_history_24)
-                    tab.icon?.setTint(ContextCompat.getColor(this, R.color.teal_700))
-                    tab.icon?.alpha = 70
+                    setIconColor(tab, R.color.teal_700)
+                    setIconAlpha(tab, 70)
                 }
             }
         }.attach()
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
-                tab.icon?.alpha = 250
+                setIconAlpha(tab, 250)
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {
-                tab.icon?.alpha = 70
+                setIconAlpha(tab, 70)
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
